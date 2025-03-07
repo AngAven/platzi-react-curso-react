b5a546ba283bbbb29ecb0a83b3d593a4e03a047c
Angel Avendaño Cruz <angel.aven.c@gmail.com>
2025-03-07 02:02:38 -0600

    Starting React Router Integration

--- src/customHooks/useLocalStorage.js
+++ src/customHooks/useLocalStorage.js
@@ -1,5 +1,4 @@
 import {useEffect, useState} from "react";
-import {makeTodos} from "../fakes/todosFake";
 
 const useLocalStorage = (itemName, initialValue) => {
     const [item, setItem] = useState(initialValue)
